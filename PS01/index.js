3f124d9ec1c84fea4bee97a7b944e7ae8b4379fb
askelton1 <askelton1@github.com>
2017-10-10 17:57:18 -0400

    PS01

--- PS01/index.js
+++ PS01/index.js
@@ -1,3 +1,51 @@
-var svg = d3.select('svg');
+var svg = d3.select('svg')
+    .append('g')
+    .attr('transform','translate(100,100)');
 
-/* Your code goes here */
+d3.csv('./data for final.csv', function(dataIn) {
+    console.log(dataIn);
+
+    svg.selectAll('circle')
+        .data(dataIn)
+        .enter()
+        .append('circle')
+        .attr('cx', function(d){
+            return scaleX(d.x)
+        })
+        .attr('cy', function(d){
+            return scaleY(d.y)
+        })
+        .attr('r', 10);
+
+    svg.append('g')
+        .attr('transform','translate(0,400)')
+        .call(d3.axisBottom(scaleX));
+
+    svg.append('g')
+        .call(d3.axisLeft(scaleY));
+
+    svg.append('text')
+        .attr('x',150)
+        .attr('y',0)
+        .attr('font-size',24)
+        .text('Marijuana Laws Across the U.S.');
+
+    svg.append('text')
+        .attr('x',300)
+        .attr('y',450)
+        .attr('font-size',16)
+        .text('YEAR');
+
+    svg.append('text')
+        .attr('x',-200)
+        .attr('y',-50)
+        .attr('font-size',16)
+        .text('STATE')
+        .attr('transform','rotate(270)')
+
+
+});
+
+var scaleX = d3.scaleLinear().domain([0,400]).range([0,600]);
+var scaleY = d3.scaleLinear().domain([500,0]).range([0,400]);
+
